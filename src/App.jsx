19714332a242e7ea6ea5eca69744dd7fce402e0d
Andrew Mandrichuk <andrew.mandrichuk2007@gmail.com>
2023-11-07 19:10:40 +0100

    Creating design for the main page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Jumbotron from "./components/Jumbotron";
+import SoundSection from "./components/SoundSection";
+import DisplaySection from "./components/DisplaySection";
+import WebgiViewer from "./components/WebgiViewer";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Jumbotron />
+      <SoundSection />
+      <DisplaySection />
+      <WebgiViewer />
+    </div>
+  );
+}
+
+export default App;
